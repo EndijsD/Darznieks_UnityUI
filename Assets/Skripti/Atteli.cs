23a6c34eb8bee414282c3cacbcbf4ec801d25b2c
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-03-12 17:36:34 +0200

    Izveidota izkrītošā saraksta poga, lai varētu izvēlēties starp diviem tēliem

---
+++ Assets/Skripti/Atteli.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Atteli : MonoBehaviour
+{
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+
+    public void dropdown(int skaitlis)
+    {
+        if(skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        } 
+        else if(skaitlis == 1)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        }
+    }
+}
