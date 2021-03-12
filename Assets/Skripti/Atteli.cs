19f553e4e27e0f255a0413b2e8773dd2b8e14376
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-03-12 21:37:03 +0200

    Izveidotas vairākas apģērbu kategorijas, kurās ir iespējams paslēpt un parādīt attiecīgos elementus ar toggle pogu

--- Assets/Skripti/Atteli.cs
+++ Assets/Skripti/Atteli.cs
@@ -7,6 +7,7 @@ public class Atteli : MonoBehaviour
 {
     public GameObject mainigaisAttels;
     public Sprite[] atteluMasivs;
+    public GameObject[] aksesuaruAtteli, cepuruAtteli, krekluAtteli, jakuAtteli, biksuAtteli, kurpjuAtteli;
 
     public void dropdown(int skaitlis)
     {
@@ -18,5 +19,84 @@ public class Atteli : MonoBehaviour
         {
             mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
         }
+    }
+
+    public void paraditPirmoA(bool vertiba)
+    {
+        aksesuaruAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroA(bool vertiba)
+    {
+        aksesuaruAtteli[1].SetActive(vertiba);
+    }
+    public void paraditTresoA(bool vertiba)
+    {
+        aksesuaruAtteli[2].SetActive(vertiba);
+    }
+
+
+    public void paraditPirmoC(bool vertiba)
+    {
+        cepuruAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroC(bool vertiba)
+    {
+        cepuruAtteli[1].SetActive(vertiba);
+    }
+    public void paraditTresoC(bool vertiba)
+    {
+        cepuruAtteli[2].SetActive(vertiba);
+    }
+
+
+    public void paraditPirmoKreklu(bool vertiba)
+    {
+        krekluAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroKreklu(bool vertiba)
+    {
+        krekluAtteli[1].SetActive(vertiba);
+    }
+
+
+    public void paraditPirmoJ(bool vertiba)
+    {
+        jakuAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroJ(bool vertiba)
+    {
+        jakuAtteli[1].SetActive(vertiba);
+    }
+    public void paraditTresoJ(bool vertiba)
+    {
+        jakuAtteli[2].SetActive(vertiba);
+    }
+
+
+    public void paraditPirmoB(bool vertiba)
+    {
+        biksuAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroB(bool vertiba)
+    {
+        biksuAtteli[1].SetActive(vertiba);
+    }
+    public void paraditTresoB(bool vertiba)
+    {
+        biksuAtteli[2].SetActive(vertiba);
+    }
+
+
+    public void paraditPirmoK(bool vertiba)
+    {
+        kurpjuAtteli[0].SetActive(vertiba);
+    }
+    public void paraditOtroK(bool vertiba)
+    {
+        kurpjuAtteli[1].SetActive(vertiba);
+    }
+    public void paraditTresoK(bool vertiba)
+    {
+        kurpjuAtteli[2].SetActive(vertiba);
     }
 }
---
+++ Assets/Skripti/Toggle.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour
+{
+    public GameObject[] aksesuari, cepures, krekli, jakas, bikses, kurpes;
+    int i;
+
+    public void Aksesuari(bool vertiba)
+    {
+        i = 0;
+        while (i != 3)
+        {
+            aksesuari[i].SetActive(vertiba);
+            i++;
+        }
+    }
+
+    public void Cepures(bool vertiba)
+    {
+        i = 0;
+        while (i != 3)
+        {
+            cepures[i].SetActive(vertiba);
+            i++;
+        }
+    }
+
+    public void Krekli(bool vertiba)
+    {
+        i = 0;
+        while (i != 2)
+        {
+            krekli[i].SetActive(vertiba);
+            i++;
+        }
+    }
+
+    public void Jakas(bool vertiba)
+    {
+        i = 0;
+        while (i != 3)
+        {
+            jakas[i].SetActive(vertiba);
+            i++;
+        }
+    }
+
+    public void Bikses(bool vertiba)
+    {
+        i = 0;
+        while (i != 3)
+        {
+            bikses[i].SetActive(vertiba);
+            i++;
+        }
+    }
+
+    public void Kurpes(bool vertiba)
+    {
+        i = 0;
+        while (i != 3)
+        {
+            kurpes[i].SetActive(vertiba);
+            i++;
+        }
+    }
+}
