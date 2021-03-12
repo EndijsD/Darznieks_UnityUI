0b5e55614599eab9a650a0ac9b6a0787fa76259a
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-03-12 16:21:37 +0200

    Pievienoju iespēju ievadīt tēla vārdu un vecumu un ar pogas palīdzību attēlot vērtības teksta laukā

---
+++ Assets/Skripti/Teksts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teksts : MonoBehaviour
+{
+    string vards, vecums;
+    public GameObject ievadesLauksVards, ievadesLauksVecums, kurAtteloTekstu;
+
+    public void attelotTekstu()
+    {
+        vards = ievadesLauksVards.GetComponent<Text>().text;
+        vecums = ievadesLauksVecums.GetComponent<Text>().text;
+        
+        if(vards != "" && vecums != "")
+        kurAtteloTekstu.GetComponent<Text>().text = vards + " ir " + vecums + " gadus vecs!";
+    }
+}
